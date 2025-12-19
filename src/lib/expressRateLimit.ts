d4fab90b6c581e81ec3c190a0089bf5ff6690341
Julia Wilson <juliawilson757@gmail.com>
2025-12-20 01:52:25 +0630

    fix: expressRateLimit.ts

--- src/lib/expressRateLimit.ts
+++ src/lib/expressRateLimit.ts
@@ -1,7 +1,7 @@
 
 import { rateLimit } from 'express-rate-limit';
 
-export const limiter = rateLimit({
+const limiter = rateLimit({
     windowMs: 60000, //1 minute time window for request limiting
     limit: 60,
     standardHeaders: 'draft-8',
@@ -10,3 +10,5 @@ export const limiter = rateLimit({
    
 
 });
+
+export default limiter

db9fe60470becf5aa1d1c505ccbbbd45a0b6bf1c
Julia Wilson <juliawilson757@gmail.com>
2025-12-22 01:17:35 +0630

    Feat: add mongodb-database-connect to the app and update the .env file

--- src/index.ts
+++ src/index.ts
@@ -27,6 +27,7 @@ app.get('/', (_req: Request, res: Response) => {
   
 });
 
-app.listen(port, () =>
-  console.log(`Server is running on http://localhost:${port}`),
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+}
 );
